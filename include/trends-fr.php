16d411e04ef833fe1878aa3b458a5bed3546e916
Léo Taillard <leo.taillard@gmail.com>
2014-11-01 16:36:34 +0100

    1add

--- include/trends-fr.php
+++ include/trends-fr.php
@@ -7,6 +7,6 @@
 		<li><a href="img/trends/LOOK-03.png" class="image-link"><img src="img/trends/LOOK-03.png" alt=""></a></li>
 		<li><a href="img/trends/04.png" class="image-link"><img src="img/trends/04.png" alt=""></a></li>
 		<li><a href="img/trends/LOOK-04.png" class="image-link"><img src="img/trends/LOOK-04.png" alt=""></a></li>
-		<li><a href="img/trends/08.png" class="image-link"><img src="img/trends/08.png" alt=""></a></li>
+		<li><a href="img/trends/08.png" class="image-link"><img src="img/trends/05.png" alt=""></a></li>
 		<li><a href="img/trends/LOOK-05.png" class="image-link"><img src="img/trends/LOOK-05.png" alt=""></a></li>
 	</ul>
