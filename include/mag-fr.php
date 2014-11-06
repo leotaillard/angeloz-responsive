761402d348f5340c55c2da17b429d30ef09168f2
Léo Taillard <leo.taillard@gmail.com>
2014-11-06 16:25:37 +0100

    das

---
+++ include/mag-fr.php
@@ -0,0 +1,76 @@
+      <ul class="gallery-mag gallery clearfix">
+
+
+        <!-- ICI C'EST FRIBOURG -->
+        <li class="ajax-popup-link" href="ajax-contents/horaire-fribourg.html">
+          <img src="img/mag/mini/mini-contact-fribourg.jpg" alt="horaire et contact fribourg"/>
+        </li>
+        <li class="image-link" href="img/mag/FRIBOURG/fribourg-4.jpg">
+          <img src="img/mag/mini/mini-fribourg-4.jpg" alt="magasin fribourg"/>
+        </li>
+
+        <li class="ajax-popup-link" href="ajax-contents/inside-fribourg.html">
+          <img src="img/mag/mini/inside-fribourg.jpg" alt="inside fribourg"/>
+        </li>
+        <li class="image-link" href="img/mag/FRIBOURG/fribourg-1.jpg">
+          <img src="img/mag/mini/mini-fribourg-1.jpg" alt="magasin fribourg"/>
+        </li>
+
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-2.jpg"></li>
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-3.jpg"></li>
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-5.jpg"></li>
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-6.jpg"></li>
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-7.jpg"></li>
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-8.jpg"></li>
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-9.jpg"></li>
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-10.jpg"></li>
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-11.jpg"></li>
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-12.jpg"></li>
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-13.jpg"></li>
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-14.jpg"></li>
+        <li class="image-link invisible" href="img/mag/FRIBOURG/fribourg-15.jpg"></li>
+      </ul>
+      <ul class="gallery-mag gallery clearfix">
+
+        <!-- ICI C'EST AVRY -->
+
+        <li class="ajax-popup-link" href="ajax-contents/horaire-avry.html">
+          <img src="img/mag/mini/mini-contact-avry.jpg" alt="horaire et contact avry"/>
+        </li>
+
+        <li class="image-link" href="img/mag/avry/avry-4.jpg">
+          <img src="img/mag/mini/mini-avry-4.jpg" alt="magasin avry"/>
+        </li>
+
+        <li class="image-link" href="img/mag/avry/avry-2.jpg">
+          <img src="img/mag/mini/mini-avry-2.jpg" alt="magasin avry"/>
+        </li>
+        <li class="ajax-popup-link" href="ajax-contents/inside-avry.html">
+          <img src="img/mag/mini/inside-avry.jpg" alt="inside avry"/>
+        </li>
+
+      </ul>
+      <ul class="gallery-mag gallery clearfix">
+
+        <!-- ICI C'EST BULLE -->
+        <li class="ajax-popup-link" href="ajax-contents/horaire-bulle.html">
+          <img src="img/mag/mini/mini-contact-bulle.jpg" alt="horaire et contact bulle" />
+        </li>
+
+
+        <li class="ajax-popup-link" href="ajax-contents/inside-bulle.html">
+          <img src="img/mag/mini/inside-bulle.jpg" alt="inside bulle"/>
+        </li>
+        <li class="image-link" href="img/mag/bulle/bulle-1.jpg">
+          <img src="img/mag/mini/mini-bulle-1.jpg" alt="magasin bulle"/>
+        </li>
+        <li class="image-link" href="img/mag/bulle/bulle-1.jpg">
+          <img src="img/mag/mini/mini-bulle-2.jpg" alt="magasin bulle"/>
+        </li>
+
+
+
+
+      </ul>
+    </div>
+
