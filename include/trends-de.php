bb6c09c078dc78ed651ce5c24a0115fcedbcb876
Léo Taillard <leo.taillard@gmail.com>
2014-11-23 21:31:16 +0100

    merge christmas

--- include/trends-de.php
+++ include/trends-de.php
@@ -1,12 +1,41 @@
 	<ul class="gallery trends clearfix">
-		<li><a href="../img/trends/01.jpg" class="image-link"><img src="../img/trends/01.jpg" alt=""></a></li>
-		<li><a href="../img/trends/LOOK-01.jpg" class="image-link"><img src="../img/trends/LOOK-01.jpg" alt=""></a></li>
-		<li><a href="../img/trends/02.jpg" class="image-link"><img src="../img/trends/02.jpg" alt=""></a></li>
-		<li><a href="../img/trends/LOOK-02.jpg" class="image-link"><img src="../img/trends/LOOK-02.jpg" alt=""></a></li>
-		<li><a href="../img/trends/03.jpg" class="image-link"><img src="../img/trends/03.jpg" alt=""></a></li>
-		<li><a href="../img/trends/LOOK-03.jpg" class="image-link"><img src="../img/trends/LOOK-03.jpg" alt=""></a></li>
-		<li><a href="../img/trends/04.jpg" class="image-link"><img src="../img/trends/04.jpg" alt=""></a></li>
-		<li><a href="../img/trends/LOOK-04.jpg" class="image-link"><img src="../img/trends/LOOK-04.jpg" alt=""></a></li>
-		<li><a href="../img/trends/08.jpg" class="image-link"><img src="../img/trends/05.jpg" alt=""></a></li>
-		<li><a href="../img/trends/LOOK-05.jpg" class="image-link"><img src="../img/trends/LOOK-05.jpg" alt=""></a></li>
+		<li><img src="../img/christmas/pattern.jpg" alt="Pattern angeloz"></li>
+		<li><a href="../img/christmas/cover-album-women.jpg" class="image-link"><img src="../img/christmas/cover-album-women.jpg" alt="Pattern angeloz"></a></li>
+		<li><img src="../img/christmas/pattern.jpg" alt="Pattern angeloz"></li>
+		<li><img src="../img/christmas/pattern.jpg" alt="Pattern angeloz"></li>
+		<li><img src="../img/christmas/pattern.jpg" alt="Pattern angeloz"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-2.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-3.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-5.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-6.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-7.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-8.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-9.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-10.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-11.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-12.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-13.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-14.jpg"></li>
+	</ul>
+
+	<ul class="gallery trends clearfix">
+
+		<li><img src="../img/christmas/pattern.jpg" alt="Pattern angeloz"></li>
+		<li><img src="../img/christmas/pattern.jpg" alt="Pattern angeloz"></li>
+		<li><img src="../img/christmas/pattern.jpg" alt="Pattern angeloz"></li>
+		<li><a href="../img/christmas/cover-album-men.jpg" class="image-link"><img src="../img/christmas/cover-album-men.jpg" alt="Pattern angeloz"></a></li>
+		<li><img src="../img/christmas/pattern.jpg" alt="Pattern angeloz"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-2.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-3.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-5.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-6.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-7.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-8.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-9.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-10.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-11.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-12.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-13.jpg"></li>
+        <li class="image-link invisible" href="../img/mag/fribourg/fribourg-14.jpg"></li>
+
 	</ul>
